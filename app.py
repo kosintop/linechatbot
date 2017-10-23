04a2ebf60bb4ca86b818802fe5e549c20bca6349
Kosin-PC\Kosin <kosin@outlook.com>
2017-10-23 23:10:08 +0700

    test 6

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def callback():
 @app.route("/test", methods=['POST'])
 def test_endpoint():
     print("testing")
-    return send_file(io.BytesIO(request.data),mimetype='image/*')
+    return send_file(io.BytesIO(request.data),mimetype='image/jpeg')
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
