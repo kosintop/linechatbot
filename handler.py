4aa8482eaf4f6d2d49c58dc00aff246e80fcf482
Kosin-PC\Kosin <kosin@outlook.com>
2017-10-24 20:06:57 +0700

    test3

--- handler.py
+++ handler.py
@@ -29,9 +29,10 @@ def handle_image_message(event):
     print(event.message.id)
     message_content = line_bot_api.get_message_content(event.message.id)
     content = message_content.content
-
+    print('posting to test')
+    requests.post("https://line-chatbot-kos.herokuapp.com/test", files={'file': content})
     headers = {'Content-type': 'application/x-www-form-urlencoded'}
-    requests.post("http://inventech.co.th/dbo_stonline/B2BSERVICES.svc/POSTIMAGE", headers=headers, files={'file': ('test.jpg',content,'application/octet-stream')}, timeout=20)
+    # requests.post("http://inventech.co.th/dbo_stonline/B2BSERVICES.svc/POSTIMAGE", headers=headers, files={'file': ('test.jpg',content,'application/octet-stream')}, timeout=20)
 
 
 @event_handler.add(MessageEvent,message=[LocationMessage])
