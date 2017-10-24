9c07a47a9788b61c401a94642e79d13b4331e59d
Kosin-PC\Kosin <kosin@outlook.com>
2017-10-24 20:21:02 +0700

    add userid to image handler

--- handler.py
+++ handler.py
@@ -5,8 +5,10 @@ from linebot.models import AudioMessage
 from linebot.models import ImageMessage
 from linebot.models import MessageEvent
 from linebot.models import TextMessage
+from linebot.models import TextSendMessage
 from linebot.models import VideoMessage
 from linebot.models import LocationMessage
+from urllib3.exceptions import MaxRetryError
 
 from settings import CHANNEL_SECRET, CHANNEL_TOKEN
 
@@ -16,32 +18,43 @@ event_handler = WebhookHandler(CHANNEL_SECRET)
 
 @event_handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
-    data = {
+    json = {
         "USERID": event.source.user_id,
         "MESSAGE": event.message.text,
         "TOKENID": event.reply_token
     }
-    requests.post("http://inventech.co.th/dbo_stonline/B2BSERVICES.svc/ASKBOBV2",json=data, timeout=20)
+    try:
+        requests.post("http://inventech.co.th/dbo_stonline/B2BSERVICES.svc/ASKBOBV2",json=json, timeout=20)
+    except MaxRetryError:
+        line_bot_api.push_message(event.source.user_id,[TextSendMessage(text='Error getting data from inventech, please retry')])
 
 
 @event_handler.add(MessageEvent,message=[ImageMessage])
 def handle_image_message(event):
+    json = {
+        "USERID": event.source.user_id,
+        "TOKENID": event.reply_token
+    }
     message_content = line_bot_api.get_message_content(event.message.id)
     content = message_content.content
     headers = {'Content-type': 'application/x-www-form-urlencoded'}
-    requests.post("http://inventech.co.th/dbo_stonline/B2BSERVICES.svc/POSTIMAGE", headers=headers,  data=content, timeout=20)
+    try:
+        requests.post("http://inventech.co.th/dbo_stonline/B2BSERVICES.svc/POSTIMAGE", headers=headers, json=json, data=content, timeout=20)
+    except MaxRetryError:
+        line_bot_api.push_message(event.source.user_id,[TextSendMessage(text='Error getting data from inventech, please retry')])
 
 
 @event_handler.add(MessageEvent,message=[LocationMessage])
 def handle_location_message(event):
-    data = {
+    json = {
         "USERID": event.source.user_id,
         "TOKENID": event.reply_token,
-		"title":event.message.title,
-		"address":event.message.address,
-		"latitude":event.message.latitude,
-		"longitude":event.message.longitude,
+        "title":event.message.title,
+        "address":event.message.address,
+        "latitude":event.message.latitude,
+        "longitude":event.message.longitude,
     }
-    r = requests.post("http://inventech.co.th/dbo_stonline/B2BSERVICES.svc/ASKBOBV2_LOCATION",json=data, timeout=20)
-    print(data)
-    print(r.content)
+    try:
+        requests.post("http://inventech.co.th/dbo_stonline/B2BSERVICES.svc/ASKBOBV2_LOCATION",json=data, timeout=20)
+    except:
+        line_bot_api.push_message(event.source.user_id,[TextSendMessage(text='Error getting data from inventech, please retry')])
