5c0789db4ac3b6c3e2c4ae80e7e836e0401178e4
DESKTOP-4TKEO8D\Antinode <kosin@outlook.com>
2017-10-24 12:45:26 +0700

    add post image url, add draft of location api

--- handler.py
+++ handler.py
@@ -6,6 +6,7 @@ from linebot.models import ImageMessage
 from linebot.models import MessageEvent
 from linebot.models import TextMessage
 from linebot.models import VideoMessage
+from linebot.models import LocationMessage
 
 from settings import CHANNEL_SECRET, CHANNEL_TOKEN
 
@@ -23,9 +24,19 @@ def handle_text_message(event):
     requests.post("http://inventech.co.th/dbo_stonline/B2BSERVICES.svc/ASKBOBV2",json=data)
 
 
-@event_handler.add(MessageEvent,message=[ImageMessage, VideoMessage, AudioMessage])
+@event_handler.add(MessageEvent,message=[ImageMessage])
 def handle_image_message(event):
     message_content = line_bot_api.get_message_content(event.message.id)
     content = message_content.content
     headers = {'Content-type': message_content.content_type}
-    requests.post('https://line-chatbot-kos.herokuapp.com/test', headers=headers, files={'image': content})
+    requests.post('"http://inventech.co.th/dbo_stonline/B2BSERVICES.svc/POSTIMAGE"', headers=headers, files={'image': content})
+
+
+@event_handler.add(MessageEvent,message=[LocationMessage])
+def handle_image_message(event):
+    data = {
+        "USERID": event.source.user_id,
+        "MESSAGE": event.message.text,
+        "TOKENID": event.reply_token
+    }
+    # requests.post("http://inventech.co.th/dbo_stonline/B2BSERVICES.svc/ASKBOBV2",json=data)
