3f9d79b98b4dfe3ff99f413eb6ab986af4b69287
Kosin-PC\Kosin <kosin@outlook.com>
2017-10-26 15:45:22 +0700

    test

--- handler.py
+++ handler.py
@@ -26,7 +26,7 @@ def handle_text_message(event):
     }
     try:
         r = requests.post("http://inventech.co.th/dbo_stonline/B2BSERVICES.svc/ASKBOBV2",json=json, timeout=20)
-        data = r.json()
+        data = r.json()['STATUS']
         print(data)
         messages = create_message(data['messages'])
         print(messages)
