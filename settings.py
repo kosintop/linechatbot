25d2f9a55a04dc86f0e84e1f9ed2bb599701ebe2
Kosin-PC\Kosin <kosin@outlook.com>
2018-02-23 21:03:18 +0700

    test http

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
-API_URL='https://inventech.co.th/dbo_stonline/B2BSERVICES.svc'
+API_URL='http://122.155.162.24/dbo_stonline/B2BSERVICES.svc'
 CHANNEL_TOKEN='ginHThu+DFgxLuQn2XBngfaT945zgOZa7h2pklDSzbE2FBf24PgsrVGwcUR3gQqVy8YjIVy84SGrP1yjcBczVuw5fgZQVPb7VuDnciOdECUc6p5NJy5pJZYxvFd2QeIby/76cpdz2SjBP+YUYWSHFgdB04t89/1O/w1cDnyilFU='
 CHANNEL_SECRET='1ecfcd57b77b5a58dfe8ae57d98b63f4'
